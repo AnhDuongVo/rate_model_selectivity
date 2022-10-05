9cd250e3bedbf0de9726b863f0ef9721092f40ce
Jieran-S <jiersun99@gmail.com>
2022-10-05 17:01:15 +0200

    add new weight learning rule

--- Implementation/integration_methods.py
+++ Implementation/integration_methods.py
@@ -42,7 +42,6 @@ def update_network(x, kwargs):
     # linearized change amount
     return timescale*update
 
-
 def nonlearning_weights(x, kwargs):
     return np.zeros_like(x)
 
@@ -52,6 +51,7 @@ def BCM_rule(weights_project, kwargs):
     activity_current=activity_all[-1]
     inputs=kwargs['Input']
     
+    # Law and Cooper E[y^2]
     tresholds=np.var(activity_all, axis=0) + np.mean(activity_all, axis=0)**2
     activity_presyn = activity_current*(activity_current-tresholds)
     weight_change=np.dot(activity_presyn[:,None], inputs[None,:])/(tresholds[:,None])
@@ -71,18 +71,36 @@ def BCM_rule_sliding_th(weights_project, kwargs):
         return thresholds
     
     timescale_learn=kwargs['tau_learn']
-    activity_all=kwargs['prev_act']    
+    activity_all=kwargs['prev_act']
     inputs=kwargs['Input']
     activity_current=activity_all[-1]
-    thresholds=threshold_function(activity_all=np.asarray(activity_all), 
+    thresholds=threshold_function(activity_all=np.asarray(activity_all),
                                 dt=kwargs['delta_t'],
                                 timescale=1./(kwargs['tau_threshold']))
     activity_postsyn = activity_current*(activity_current-thresholds)
     weight_change=np.dot(activity_postsyn[:,None], inputs[None,:])/(thresholds[:,None])
     return timescale_learn*weight_change*kwargs['w_struct_mask']
+
+
+def BCM_rule_InC(weight_project, kwargs):
+    def threshold_function(activity_all, dt, timescale):
+        number_timepoints=len(activity_all)
+        #integral_factors  
+        prefactors_inside_integral_pre=np.arange(-1*number_timepoints+1,1)*dt
+        prefactors_inside_integral=np.exp(prefactors_inside_integral_pre*timescale)
+        thresholds=timescale*integrate.simps(y=prefactors_inside_integral[:,None]*activity_all**2,x=None, dx=dt, axis=0)
+        return thresholds
     
-
-
-
-
-
+    timescale_learn=kwargs['tau_learn']
+    activity_all=kwargs['prev_act']
+    inputs=kwargs['Input']
+    nonlinearity = kwargs['nonlinearity']
+    activity_current=activity_all[-1]
+    thresholds=threshold_function(activity_all=np.asarray(activity_all),
+                                dt=kwargs['delta_t'],
+                                timescale=1./(kwargs['tau_threshold']))
+    activity_postsyn = activity_current*(activity_current-thresholds)
+    # maybe should use the derivative of the weight change function
+    weight_change=np.dot(np.dot(activity_postsyn[:,None], inputs[None,:]), 
+                            nonlinearity(activity_current))
+    return timescale_learn*weight_change*kwargs['w_struct_mask']
